e48718a0f2d4fc624368dce59d303bc987f44bdf
tfermoso <tomas.fermoso@gmail.com>
2024-04-25 20:41:04 +0200

    ajustando estilo para moviles

--- index.php
+++ index.php
@@ -18,14 +18,18 @@ $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>App Orders</title>
 </head>
 
 <body>
+    <div class="container contenedor-productos row">
+    <h3>Productos</h3>
+
     <?php
     // Mostrar los resultados
     foreach ($resultados as $product) {
-        echo '<div class="card" style="width: 18rem;">
+        echo '<div class="card productcard col-md-3 col-sm-12" ">
         <img src="assets/product/' . $product["image"] . '" class="card-img-top" alt="...">
         <div class="card-body">
         <div class="producto-detalle">
@@ -39,13 +43,13 @@ $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
           </div>
           <div class="add-to-cart">
             <input class="form-control" type="number" name="" id="">
-            <a href="#" class="btn btn-primary">Add to Cart</a>
+            <a href="#" class="btn btn-primary"><i class="fa-solid fa-cart-plus"></i></a>
           </div>
         </div>
       </div>';
     }
     ?>
-<input type="number" name="" id="">
+     </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
