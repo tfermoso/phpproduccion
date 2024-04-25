29bc171a3ac993f6bac569538a76da24d5e16f78
tfermoso <tomas.fermoso@gmail.com>
2024-04-25 19:51:48 +0200

    cargamos hoja de estilos propia

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once("conexion.php");
-$sql="select * from product";
+$sql = "select * from product";
 $consulta = $conn->prepare($sql);
 // Ejecutar la consulta
 $consulta->execute();
@@ -9,29 +9,37 @@ $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <title>App Orders</title>
+</head>
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-  <?php
+<body>
+    <?php
     // Mostrar los resultados
     foreach ($resultados as $product) {
-        $card='<div class="card" style="width: 18rem;">
-        <img src="assets/product/'.$product["image"].'" class="card-img-top" alt="...">
+        echo '<div class="card" style="width: 18rem;">
+        <img src="assets/product/' . $product["image"] . '" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">'.$product["name"].'</h5>
-          <p class="card-text">'.$product["description"].'</p>
+        <div class="producto-detalle">
+            <div>  
+                <h5 class="card-title">' . $product["name"] . '</h5>
+                <p class="card-text">' . $product["description"] . '</p>
+            </div>
+            <div>
+                <h5 class="card-title">' . $product["price"] . '€/kg</h5>
+            </div>
+          </div>
           <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>';
-    echo $card;
     }
     ?>
 
@@ -45,5 +53,6 @@ $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
+
 </html>
