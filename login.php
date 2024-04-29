de0c9cb4cd8279131b6dc9b47bd221a6e2e7733a
tfermoso <tomas.fermoso@gmail.com>
2024-04-29 17:31:49 +0200

    login

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+if(isset($_POST["email"])){
+    try{
+        require_once("conexion.php");
+        $email=$_POST["email"];
+        $password=$_POST["password"];
+        $sql="select * from user where email=? and password=?";
+        $stm=$conn->prepare($sql);
+        $stm->bindParam(1,$email);
+        $stm->bindParam(2,$password);
+        $stm->execute();
+        if($stm->rowCount()>0){
+            $result=$stm->fetchAll(PDO::FETCH_ASSOC);
+            $username=$result[0]["username"];
+            session_start();
+            $_SESSION["username"]=$username;
+            header("Location: ./");
+            exit();
+        }else{
+            $error="Usuario o Contraseña incorrecto";
+        }
+    }catch(Exception $e){
+        $error="Error interno ".$e->getMessage();
+    }
+    
+}
+?>
+
+<?php
+if(isset($error)){
+    echo $error;
+}
+?>
+
