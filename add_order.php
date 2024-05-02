9d0eda1e2665127e8b68c292d344c6110f938175
tfermoso <tomas.fermoso@gmail.com>
2024-05-02 20:09:20 +0200

    creamos pedido

--- add_order.php
+++ add_order.php
@@ -1,2 +1,28 @@
 <?php
 session_start();
+if(isset($_SESSION["username"]) && isset($_SESSION["idcart"])){
+    $iduser=$_SESSION["iduser"];
+    $idcart=$_SESSION["idcart"];
+    if(isset($_POST["date"])){
+        $date=$_POST["date"];
+        $idaddress=$_POST["idaddress"];
+        include("conexion.php");
+        $sql="insert into `order` (deliverydate,idcart,idaddress) values (?,?,?)";
+        $stm=$conn->prepare($sql);
+        $stm->bindParam(1,$date);
+        $stm->bindParam(2,$idcart);
+        $stm->bindParam(3,$idaddress);
+        $stm->execute();
+        if($stm->rowCount()>0){
+            //Pendiente borrar datos de session
+
+            header("Location: ./");
+            exit();
+        }
+
+    }else{
+        header("Location: ./");
+    }
+}else{
+    header("Location: ./");
+}
